7f142bf2d7fd83f6bc018b5669edfde5f7867e53
minsangkimme <minsang.tech@gmail.com>
2019-08-19 11:30:34 +0900

    Hotel App Upload (console.Ver)

---
+++ JavaScript/hotel-app.js
@@ -0,0 +1,45 @@
+const Hotel = {
+  // 고객 리스트
+  _name: [],
+
+  // 체크인
+  checkIn: function(name) {    
+    if (this._name.indexOf(name) > -1) return `${name}님은 이미 체크인 상태입니다.`
+
+    const valueCheck =
+      typeof name === 'string' && name.length > 0
+        ? this._name.push(name)
+        : false;
+
+    if (!valueCheck) return 'ERROR: Type Or Length Error';
+
+    return `안녕하세요. ${this._name[valueCheck - 1]}님 반갑습니다`;
+  },
+
+  // 체크아웃 
+  checkOut: function(name) {
+    if (this._name.indexOf(name) === -1) return '없는 고객입니다.';
+
+    let guest = []; // this._name 복제용도
+    const valueCheck =
+      typeof name === 'string' && name.length > 0
+        ? this._name.indexOf(name)
+        : false;
+
+    guest = this._name.slice();
+    this._name.splice(valueCheck, 1);
+
+    if (valueCheck > -1) return `${guest[valueCheck]}님 감사합니다. 안녕히 가세요`;
+    if (!valueCheck) return 'ERROR: Type Or Length Error';
+  },
+
+  // 고객 상태체크
+  getStatus: function() {
+    const status =
+      this._name.length > 0
+        ? `${this._name}님이 체크인했습니다.`
+        : '고객이 없습니다';
+    return status;
+  },
+  
+};
