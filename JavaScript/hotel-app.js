b01cb078661b5510f3f95c2bfa219457905b4b9c
minsangkimme <minsang.tech@gmail.com>
2019-08-26 17:56:49 +0900

    수정사항 없음

--- JavaScript/hotel-app.js
+++ JavaScript/hotel-app.js
@@ -4,13 +4,9 @@ const Hotel = {
 
   // 체크인
   checkIn: function(name) {
-    if (this._name.indexOf(name) > -1)
-      return `${name}님은 이미 체크인 상태입니다.`;
+    if (this._name.indexOf(name) > -1) return `${name}님은 이미 체크인 상태입니다.`;
 
-    const valueCheck =
-      typeof name === 'string' && name.length > 0
-        ? this._name.push(name)
-        : false;
+    const valueCheck = typeof name === 'string' && name.length > 0 ? this._name.push(name) : false;
 
     if (!valueCheck) return 'ERROR: Type Or Length Error';
 
@@ -22,27 +18,20 @@ const Hotel = {
     if (this._name.indexOf(name) === -1) return '없는 고객입니다.';
 
     let guest = []; // this._name 복제용도
-    const valueCheck =
-      typeof name === 'string' && name.length > 0
-        ? this._name.indexOf(name)
-        : false;
+    const valueCheck = typeof name === 'string' && name.length > 0 ? this._name.indexOf(name) : false;
 
     guest = this._name.slice();
     this._name.splice(valueCheck, 1);
 
-    if (valueCheck > -1)
-      return `${guest[valueCheck]}님 감사합니다. 안녕히 가세요`;
+    if (valueCheck > -1) return `${guest[valueCheck]}님 감사합니다. 안녕히 가세요`;
     if (!valueCheck) return 'ERROR: Type Or Length Error';
   },
 
   // 고객 상태체크
   getStatus: function() {
-    const status =
-      this._name.length > 0
-        ? `${this._name}님이 체크인했습니다.`
-        : '고객이 없습니다';
+    const status = this._name.length > 0 ? `${this._name}님이 체크인했습니다.` : '고객이 없습니다';
     return status;
-  }
+  },
 };
 
 // 체크인 및 상태 확인
