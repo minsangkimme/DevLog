15f73e983692c472cfc39e9325465f4566cd995c
minsangkimme <minsang.tech@gmail.com>
2019-11-25 16:30:39 +0900

    연습

--- express-app/api-server.js
+++ express-app/api-server.js
@@ -1,33 +1,25 @@
 const express = require('express');
-const app =  express();
+const app = express();
 
 /** 설정 */
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 /** 설정 */
 
-
-const userDB = [{
-    email : "minsang@naver.com",
-    password: "123123",
-    name: "김민상"
-}];
+const userDB = [
+  {
+    email: 'minsang@naver.com',
+    password: '123123',
+    name: '김민상',
+  },
+];
 
 app.post('/register', (req, res) => {
-   res.send(userDB.push(req.body));
+  res.send(userDB.push(req.body));
 });
 
 app.get('/users', (req, res) => {
-    res.send(userDB);
+  res.send(userDB);
 });
-
-app.get('/wqeqwas', (req, res) => {
-    res.send(userDB);
-});
-
-
-
 
 app.listen(4001);
-
-
