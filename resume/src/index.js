b8c787b614f8c2f1bd871f1abbe7e99dd66be79e
minsangkimme <minsang.tech@gmail.com>
2019-12-16 18:58:08 +0900

    자기소개 페이지

---
+++ resume/src/index.js
@@ -0,0 +1,23 @@
+const portraitEl = document.querySelector('.portrait');  // bg
+
+const changeBg = function(id) {
+    if (id === 'project1') {
+        portraitEl.style.backgroundImage = `url('../public/searchgithub.jpg')`;       
+        portraitEl.style.transition = 'all 1s';
+    }
+
+    if (id === 'project2') {
+        portraitEl.style.backgroundImage = `url('../public/todos.jpg')`;       
+        portraitEl.style.transition = 'all 1s';
+    }
+
+    if (id === 'project3') {
+        portraitEl.style.backgroundImage = `url('../public/nodes.jpg')`;       
+        portraitEl.style.transition = 'all 1s';
+    }
+}
+
+const originalBg = function() {
+    portraitEl.style.backgroundImage = `url('../public/me.png')`;
+    portraitEl.style.transition = `all 1s`;
+}
